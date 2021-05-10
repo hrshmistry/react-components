b6f9e7d66d83a0f3ae678249e568620a7a9306dc
Harsh Mistry <hhrshmistry@gmail.com>
2021-05-10 20:15:06 +0530

    Conditional Rendering: ternary operator

--- src/ConditionalRendering.js
+++ src/ConditionalRendering.js
@@ -1,17 +1,10 @@
 import React from 'react'
 
 function ConditionalRendering({isLoggedIn}) {
-    let message;
-
-    if (isLoggedIn) {
-        message = <h1>you are logged in</h1>
-    } else {
-        message = <h1>please log in</h1>
-    }
 
     return (
         <div>
-            {message}
+            {isLoggedIn ? <h1>you are logged in</h1> : <h1>please log in</h1>}
         </div>
     )
 }
