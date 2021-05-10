d93fa5bbef2bc866ad3f0340282848f8ec3fa89d
Harsh Mistry <hhrshmistry@gmail.com>
2021-05-10 20:16:22 +0530

    Conditional Rendering: short circuiting

--- src/ConditionalRendering.js
+++ src/ConditionalRendering.js
@@ -4,7 +4,7 @@ function ConditionalRendering({isLoggedIn}) {
 
     return (
         <div>
-            {isLoggedIn ? <h1>you are logged in</h1> : <h1>please log in</h1>}
+            {isLoggedIn && <h1>you are logged in</h1>}
         </div>
     )
 }
