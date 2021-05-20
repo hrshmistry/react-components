e79368271aebebe74eefe8201b7d3c8b46f2c15c
Harsh Mistry <hhrshmistry@gmail.com>
2021-05-20 10:46:11 +0530

    rendering array of objects and passing key props

--- src/UserList.js
+++ src/UserList.js
@@ -1,11 +1,36 @@
 import React from 'react'
 
 function UserList() {
-    // array
-    let users = ["Riya", "Nishu", "Tanvi"]
+    // array of objects
+    let users = [
+        {
+            name: "Riya",
+            age: 21,
+            language: "javascript",
+            key: 1
+        },
+        {
+            name: "Nishu",
+            age: 21,
+            language: "C++",
+            key: 2
+        },
+        {
+            name: "Tanvi",
+            age: 21,
+            language: "Perl",
+            key: 3
+        }
+    ]
+
     return (
         <div>
-            {users.map(user => <h1>{user}</h1>)}
+            {users.map(user => {
+                return (<div key = {user.key}>
+                    <h1>Name: {user.name}</h1>
+                    <h1>Age: {user.age}</h1>
+                </div>)
+            })}
         </div>
     )
 }
